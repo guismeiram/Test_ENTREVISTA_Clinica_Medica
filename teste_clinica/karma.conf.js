7a38c6b55adc64945e794973ec93b902d71c2ad3
guismeiram <boralagui@outlook.com>
2022-05-30 07:41:54 -0300

    commitando projeto angular

--- teste_clinica/karma.conf.js
+++ teste_clinica/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/clinica'),
+      dir: require('path').join(__dirname, './coverage/teste_clinica'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- teste_clinica/src/app/app.component.spec.ts
+++ teste_clinica/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'clinica'`, () => {
+  it(`should have as title 'teste_clinica'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('clinica');
+    expect(app.title).toEqual('teste_clinica');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('clinica app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('teste_clinica app is running!');
   });
 });
