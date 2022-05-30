0f40901e297f6ef43193e234eca608e1c301d31a
guismeiram <boralagui@outlook.com>
2022-05-30 04:16:46 -0300

    first commit

---
+++ ClinicaMedicaTest/ClinicaMedicaTest/src/main/java/br/com/ClinicaMedicaTest/ClinicaMedicaTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.ClinicaMedicaTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaMedicaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaMedicaTestApplication.class, args);
+	}
+
+}
---
+++ ClinicaMedicaTest/ClinicaMedicaTest/src/test/java/br/com/ClinicaMedicaTest/ClinicaMedicaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ClinicaMedicaTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaMedicaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
